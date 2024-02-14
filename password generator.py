8b51fa6dab9d2494bbbfc030aae26d7e111d4588
KALAI-77 <159885630+KALAI-77@users.noreply.github.com>
2024-02-14 11:22:20 +0530

    password generator.py

--- password generator.py
+++ password generator.py
@@ -1,17 +1,15 @@
-import random       # python in-built function (random)
+import random                                      # python in-built function (random)
 
-capital = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"     # capital variable's string elements include in password
+capital = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"           
 
-small = "abcdefghijklmnopqrstuvwxyz"      # small variable's string elements include in password
+small = "abcdefghijklmnopqrstuvwxyz"            
 
-numbers = "0123456789"        # numbers variable's string elements include in password
+numbers = "0123456789"                           
 
-symbols = "@#$%&*?"       # symbols variable's string elements include in password
+symbols = "@#$%&*?"                              
 
 string = ( capital + small + numbers + symbols )   # this string variable has elements of capital , samll , numbers , symbols 
 
-word = random.sample( string,10 )    #  one of the random keywords is 'sample' it's giving number of password in string variable
-
-password = " ".join(word)       #  join function adds the space between each number of passwords
+password = " ".join( random.sample( string,10 ))    #  join function work with sample function & adds the space between each number of passwords
 
 print( "Your 10 Digit Password is : ",password )    #  in print statement print the 10 digit password
